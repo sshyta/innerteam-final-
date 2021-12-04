7ca19adb0f0e284d904cea9966fb7e752d8aa69a
sgustok <86688897+sshyta@users.noreply.github.com>
2021-12-04 06:11:37 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -40,10 +40,129 @@ class files (db.Model):
 db.create_all()
 
 
-@app.route('/bob')
+@app.route('/uppload')
 def file():
     return render_template('uppload.html')
 
+
+@app.route('/ExampleAccountConfirmed')
+def Account():
+    return render_template('ExampleAccountConfirmed.html')
+
+
+@app.route('/AccountDetails')
+def AccountDetails():
+    return render_template('AccountDetails.html')
+
+
+@app.route('/AccountDetailsConfirm')
+def AccountDetailsConfirm():
+    return render_template('AccountDetailsConfirm.html')
+
+
+@app.route('/ExampleAccount')
+def ExampleAccount():
+    return render_template('ExampleAccount.html')
+
+
+@app.route('/SiteSettings')
+def SiteSettings():
+    return render_template('SiteSettings.html')
+
+
+@app.route('/History')
+def History():
+    return render_template('History.html')
+
+
+@app.route('/Problems')
+def Problems():
+    return render_template('Problems.html')
+
+
+@app.route('/Exit')
+def Exit():
+    return render_template('Exit.html')
+
+
+@app.route('/Login')
+def Login():
+    return render_template('Login.html')
+
+
+@app.route('/Search')
+def Search():
+    return render_template('Search.html')
+
+
+@app.route('/PassResponse')
+def PassResponse():
+    return render_template('PassResponse.html')
+
+
+@app.route('/YouNeedLogin')
+def YouNeedLogin():
+    return render_template('YouNeedLogin.html')
+
+
+@app.route('/RequestPassLogin')
+def RequestPassLogin():
+    return render_template('RequestPassLogin.html')
+
+
+@app.route('/RequestPassReset')
+def RequestPassReset():
+    return render_template('RequestPassReset.html')
+
+
+@app.route('/PassAndLogResponse')
+def PassAndLogResponse():
+    return render_template('PassAndLogResponse.html')
+
+
+@app.route('/ProjectCard')
+def ProjectCard():
+    return render_template('ProjectCard.html')
+
+
+@app.route('/ProjectSearch')
+def ProjectSearch():
+    return render_template('ProjectSearch.html')
+
+
+@app.route('/ProjectCardSearch')
+def ProjectCardSearch():
+    return render_template('ProjectCardSearch.html')
+
+
+@app.route('/ProjectExample')
+def ProjectExample():
+    return render_template('ProjectExample.html')
+
+
+@app.route('/ProjectPiloting')
+def ProjectPiloting():
+    return render_template('ProjectPiloting.html')
+
+
+@app.route('/ProjectPilotingSearch')
+def ProjectPilotingSearch():
+    return render_template('ProjectPilotingSearch.html')
+
+
+@app.route('/ProjectAskInnov')
+def ProjectAskInnov():
+    return render_template('ProjectAskInnov.html')
+
+
+@app.route('/ProjectAskInnovSearch')
+def ProjectAskInnovSearch():
+    return render_template('ProjectAskInnovSearch.html')
+
+
+@app.route('/Organization')
+def Organization():
+    return render_template('Organization.html')
 
 
 @app.route('/uploads/<filename>')
@@ -59,7 +178,6 @@ def showPost(id_post):
     return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
 
 
-
 @app.route('/')
 def Main():
     return render_template('MainPage.html')
@@ -68,6 +186,16 @@ def Main():
 @app.route('/Search')
 def search():
     return render_template('Search.html')
+
+
+@app.route('/MainAfterSearch')
+def MainAfterSearch():
+    return render_template('MainAfterSearch.html')
+
+
+@app.route('/Search_result')
+def result():
+    return render_template('Search_result.html')
 
 
 @app.route('/SearchRes')
@@ -82,4 +210,3 @@ def MainPage():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
